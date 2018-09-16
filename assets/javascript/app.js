9d6e2089af10a6f9c9b24d4230177cac89bf76aa
Veronica L <verolozada12@gmail.com>
2018-09-16 16:06:28 -0400

    firsts questions added

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+//NOTE: cons cannot be reassigned., Let can be. 
+
+
+//create questions for the quiz with the respectives answers. 
+const questions = [
+    {
+        q1: "What's the name of Hank Pym's daughter in Ant-Man? ",
+        answers: {
+            a: "Hope Thomson",
+            b: "Hope Van Pym",
+            c: "Hope Van Dyne",
+            d: "Hope Smith"
+        },
+        correctAns: "c"
+    },
+    {
+        q2: "When was realeasded the first Spiderman Movie?",
+        answers: {
+            a: "2002",
+            b: "2005",
+            c: "2000",
+            d: "2003"
+        },
+        correctAns: "a"
+    }
+]
+
+console.log(questions);
